55c1167651a4e68eed54d3458ad8b6ae8a0dec66
seanmfox <geekypghrunner@gmail.com>
2019-05-28 20:17:29 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCalories = New System.Windows.Forms.TextBox()
+        Me.txtFat = New System.Windows.Forms.TextBox()
+        Me.lblCalories = New System.Windows.Forms.Label()
+        Me.lblFat = New System.Windows.Forms.Label()
+        Me.lblPercentage = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCalories
+        '
+        Me.txtCalories.Location = New System.Drawing.Point(300, 18)
+        Me.txtCalories.Name = "txtCalories"
+        Me.txtCalories.Size = New System.Drawing.Size(108, 22)
+        Me.txtCalories.TabIndex = 0
+        '
+        'txtFat
+        '
+        Me.txtFat.Location = New System.Drawing.Point(300, 74)
+        Me.txtFat.Name = "txtFat"
+        Me.txtFat.Size = New System.Drawing.Size(108, 22)
+        Me.txtFat.TabIndex = 1
+        '
+        'lblCalories
+        '
+        Me.lblCalories.AutoSize = True
+        Me.lblCalories.Location = New System.Drawing.Point(22, 21)
+        Me.lblCalories.Name = "lblCalories"
+        Me.lblCalories.Size = New System.Drawing.Size(262, 17)
+        Me.lblCalories.TabIndex = 2
+        Me.lblCalories.Text = "Enter the number of calories in the food:"
+        '
+        'lblFat
+        '
+        Me.lblFat.AutoSize = True
+        Me.lblFat.Location = New System.Drawing.Point(22, 77)
+        Me.lblFat.Name = "lblFat"
+        Me.lblFat.Size = New System.Drawing.Size(272, 17)
+        Me.lblFat.TabIndex = 3
+        Me.lblFat.Text = "Enter the number of fat grams in the food:"
+        '
+        'lblPercentage
+        '
+        Me.lblPercentage.AutoSize = True
+        Me.lblPercentage.Location = New System.Drawing.Point(22, 144)
+        Me.lblPercentage.Name = "lblPercentage"
+        Me.lblPercentage.Size = New System.Drawing.Size(272, 17)
+        Me.lblPercentage.TabIndex = 4
+        Me.lblPercentage.Text = "Percentage of calories that come from fat:"
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(300, 137)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 24)
+        Me.Label1.TabIndex = 5
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(36, 203)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(173, 203)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(300, 203)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(430, 252)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblPercentage)
+        Me.Controls.Add(Me.lblFat)
+        Me.Controls.Add(Me.lblCalories)
+        Me.Controls.Add(Me.txtFat)
+        Me.Controls.Add(Me.txtCalories)
+        Me.Name = "Form1"
+        Me.Text = "Fat Percentage Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCalories As TextBox
+    Friend WithEvents txtFat As TextBox
+    Friend WithEvents lblCalories As Label
+    Friend WithEvents lblFat As Label
+    Friend WithEvents lblPercentage As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
